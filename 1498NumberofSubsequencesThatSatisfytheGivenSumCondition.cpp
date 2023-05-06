1ec170ebde7f3ec7a2c218f7b1bce990bfc6afcd
Tolga-dev <orhan.konat.12@gmail.com>
2023-05-06 18:29:32 +0300

    added daily!

---
+++ 1498NumberofSubsequencesThatSatisfytheGivenSumCondition.cpp
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/number-of-subsequences-that-satisfy-the-given-sum-condition/description/
+#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#define vi vector<int>
+#define uosi vector<int>
+static const int __ = []() { std::ios::sync_with_stdio(false); std::cin.tie(nullptr); std::cout.tie(nullptr); return 0; }();
+
+static vector<int> power_of_2 = {1};
+const int mod_div = 1000000007;
+
+static const int _ = []() { for (auto i = 1; i <= (pow(10,5)); ++i)
+                                power_of_2.push_back((power_of_2.back() << 1) % mod_div); return 0;
+}();
+
+
+int numSubseq_2(vector<int>& nums, int target) {
+    int result = 0, left = 0, r = nums.size() - 1;
+
+    sort(begin(nums), end(nums));
+
+    while (left <= r) {
+        if(nums[r] + nums[left] <= target){
+            result += power_of_2[r-left];
+            if(result >= mod_div)
+                result -= mod_div;
+            ++left;
+        }
+        else
+            --r;
+    }
+
+    return result;
+}
+
+int main()
+{
+    vector<int> nums = {2,3,3,4,6,7};
+    int target = 12;
+    int res =numSubseq_2(nums,target);
+    cout << res << endl;
+}
